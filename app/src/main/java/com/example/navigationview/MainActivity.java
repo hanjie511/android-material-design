25c8c7a8e5b9749c9711709bfb4391c994fa9ec8
hanjie511 <hanjie511@gmail.com>
2019-10-06 10:42:39 +0800

    更新

--- app/src/main/java/com/example/navigationview/MainActivity.java
+++ app/src/main/java/com/example/navigationview/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.navigationview;
 
 import android.content.Context;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
@@ -8,11 +9,14 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.MenuCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -37,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList picList=new ArrayList();
     Context context;
+    MyAdapter adapter;
     FloatingActionButton floatingActionButton;
+    SwipeRefreshLayout swipeRefreshLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,27 +53,73 @@ public class MainActivity extends AppCompatActivity {
         initView();
         picList=initList();
         context=MainActivity.this;
-        MyAdapter adapter=new MyAdapter(picList,R.layout.recycler_item,context);
+        adapter=new MyAdapter(picList,R.layout.recycler_item,context);
         LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapter);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {//recyclerView的滚动监听，主要用来监听上拉加载更多
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                if (newState==RecyclerView.SCROLL_STATE_IDLE){
+                    RecyclerView.LayoutManager layoutManager=recyclerView.getLayoutManager();
+                    int lastVisiblePosition;
+                    if (layoutManager instanceof GridLayoutManager){
+                        lastVisiblePosition=((GridLayoutManager) layoutManager).findLastVisibleItemPosition();
+                    }else {
+                        lastVisiblePosition = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
+                    }
+                    if (layoutManager.getChildCount()>0             //当当前显示的item数量>0
+                            &&lastVisiblePosition>=layoutManager.getItemCount()-1           //当当前屏幕最后一个加载项位置>=所有item的数量
+                            &&layoutManager.getItemCount()>layoutManager.getChildCount()) { // 当当前总Item数大于可见Item数
+                                try{
+                                    Thread.sleep(1500);
+                                    picList.add("https://b-ssl.duitang.com/uploads/item/201601/07/20160107170247_5GX3k.jpeg");
+                                    picList.add("https://i0.hdslb.com/bfs/article/708aaa2a706bbc722e92fb28becebe680064b2bd.jpg");
+                                    picList.add("https://i0.hdslb.com/bfs/article/70a845c18dddf4f789c4a59446c917d0df1e197a.jpg");
+                                    picList.add("https://b-ssl.duitang.com/uploads/item/201610/11/20161011163941_JmvwG.thumb.700_0.jpeg");
+                                    adapter.notifyDataSetChanged();
+                                }catch (Exception e){
+                                    e.printStackTrace();
+                                }
+                            }
+                }
+            }
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
     }
     private void initView(){
-        navigationView=findViewById(R.id.nav_main);
         bottomNavigationView=findViewById(R.id.buttonNavigation);
         drawerLayout=findViewById(R.id.drawer);
-        View view= navigationView.getHeaderView(0);
+        navigationView=findViewById(R.id.nav_main);
+        View view= navigationView.getHeaderView(0);//找到navigationView的头部View，然后才能为其控件设置监听(只有这样才能为navigationView的headerView的控件设置点击事件)，
         circleImageView=view.findViewById(R.id.circleImage_nav_header);
         username=view.findViewById(R.id.username_nav_header);
         recyclerView=findViewById(R.id.recyclerView);
         floatingActionButton=findViewById(R.id.fb);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        swipeRefreshLayout=findViewById(R.id.swipeRefresh);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {//设置下拉刷新的监听
+            @Override
+            public void onRefresh() {
+                picList.clear();
+                picList=initList();
+                adapter=new MyAdapter(picList,R.layout.recycler_item,context);
+                LinearLayoutManager linearLayoutManager=new LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false);
+                recyclerView.setLayoutManager(linearLayoutManager);
+                recyclerView.setAdapter(adapter);
+                swipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(context, "数据已刷新", Toast.LENGTH_SHORT).show();
+            }
+        });
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {//设置floatingActionButton的点击v事件
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "分享被点击了", Toast.LENGTH_SHORT).show();
             }
         });
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {//设置toolbar的navigationButton的点击事件
             @Override
             public void onClick(View v) {
                 drawerLayout.openDrawer(Gravity.LEFT);
@@ -85,6 +137,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "你点击用户名", Toast.LENGTH_SHORT).show();
             }
         });
+        //底部导航栏的点击监听事件
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -109,6 +162,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        //策划导航栏的点击监听事件
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -146,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) {//toolBar的菜单的创建方法
         getMenuInflater().inflate(R.menu.toolbarmenu,menu);
         MenuItem item=menu.findItem(R.id.search_toolbar_menu);
         SearchView searchView=(SearchView)item.getActionView();
@@ -168,7 +222,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {//toolbar的菜单按钮点击监听
         switch(item.getItemId()){
             case R.id.search_toolbar_menu:
                 Toast.makeText(this, "你点击了搜索按钮", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/navigationview/MyAdapter.java
+++ app/src/main/java/com/example/navigationview/MyAdapter.java
@@ -7,12 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.VH> {
+    private final int TYPE_NORMAL=0;
+    private final int TYPE_FOOT=1;
+
     ArrayList<String> list=null;
     int layout=0;
     Context context;
@@ -28,24 +32,45 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.VH> {
         this.layout=layout;
         this.context=context;
     }
-
     @NonNull
     @Override
-    public VH onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view= LayoutInflater.from(viewGroup.getContext()).inflate(layout,viewGroup,false);
-        VH vh=new VH(view);
+    public MyAdapter.VH onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        VH vh=null;
+        if(i==TYPE_NORMAL){//为普通itemViewType时，返回普通itemView的ViewHolder
+            View view= LayoutInflater.from(viewGroup.getContext()).inflate(layout,viewGroup,false);
+            vh=new VH(view);
+        }else if(i==TYPE_FOOT){//不为普通itemViewType时，返回footerItemView的ViewHolder
+                View view=LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.footer_view,viewGroup,false);
+                vh=new VH(view);
+        }
         return vh;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull VH vh, int i) {
-        String path=list.get(i);
-        System.out.println("path:"+path);
-        Glide.with(context).load(path).into(vh.image);
+    public void onBindViewHolder(@NonNull VH viewHolder, int i) {
+        if(getItemViewType(i)==TYPE_NORMAL){//不为footerItemView时才进行数据绑定
+            VH vh=(VH)viewHolder;
+            String path=list.get(i);
+            Glide.with(context).load(path).into(vh.image);
+        }
+
     }
 
     @Override
-    public int getItemCount() {
-        return list.size();
+    public int getItemCount() {//为了显示footerView要多返回一个item
+        if(list.size()>0){
+            return list.size()+1;
+        }else{
+            return  0;
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {//根据返回的itemViewType去执行onCreateViewHolder和onBindViewHolder方法
+        if(position+1==getItemCount()){
+            return TYPE_FOOT;
+        }else{
+            return  TYPE_NORMAL;
+        }
     }
 }
